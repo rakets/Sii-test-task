a9188aa1ab5039cf03024a6c3394daf18d2b2f7e
Valiantsin Ushkevich <vushkevih@gmail.com>
2026-05-05 14:45:26 +0200

    first commit

---
+++ src/main/java/com/test_task/sii/SiiApplication.java
@@ -0,0 +1,13 @@
+package com.test_task.sii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test_task/sii/SiiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test_task.sii;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
