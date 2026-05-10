ed581932cd22f2bbec6da80686625f92853734c5
Valiantsin Ushkevich <vushkevih@gmail.com>
2026-05-11 01:18:53 +0200

    test: Update test-class 'MembershipPlanServiceTest'. - Add test 'getAllPlansForGym_ShouldReturnListOfMembershipPlanDTO' for success method 'getAllPlansForGym'. - Add test 'getAllPlansForGym_ShouldThrowEntityNotFoundException' for exception in method 'getAllPlansForGym'. - Add test 'getAllPlansForGym_ShouldThrowEntityNotFoundException' for method 'convertEntityToDTO'. - Add test 'convertDTOtoEntity_ShouldReturnMembershipPlan' for method 'convertDTOtoEntity'.

--- src/test/java/com/test_task/sii/service/MembershipPlanServiceTest.java
+++ src/test/java/com/test_task/sii/service/MembershipPlanServiceTest.java
@@ -16,6 +16,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.math.BigDecimal;
 import java.util.Currency;
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -163,5 +164,107 @@ public class MembershipPlanServiceTest {
         verify(gymRepository, times(1)).findById(gymId);
         verify(membershipPlanRepository, times(1)).existsByNameAndGym_Id(planDTO.getName(), gymId);
         verify(membershipPlanRepository, never()).save(any(MembershipPlan.class));
+    }
+
+    // test for success method 'getAllPlansForGym'
+    @Test
+    void getAllPlansForGym_ShouldReturnListOfMembershipPlanDTO() {
+        // GIVEN
+        Long gymId = 1L;
+
+        Gym gym = createGym();
+        gym.setId(gymId);
+
+        MembershipPlan plan = createMembershipPlan(gym);
+        List<MembershipPlan> membershipPlanList = List.of(plan);
+
+        GymDTO gymDTO = createGymDTO();
+
+        when(gymRepository.existsById(gymId)).thenReturn(true);
+        when(membershipPlanRepository.findByGymId(gymId)).thenReturn(membershipPlanList);
+        when(gymService.convertEntityToDTO(gym)).thenReturn(gymDTO); // Нужно для маппинга внутри convertEntityToDTO
+
+        // WHEN
+        List<MembershipPlanDTO> response = membershipPlanService.getAllPlansForGym(gymId);
+
+        // THEN
+        assertNotNull(response);
+        assertEquals(1, response.size()); // Проверяем, что размер списка совпадает
+
+        MembershipPlanDTO responseDTO = response.get(0);
+        assertEquals(plan.getName(), responseDTO.getName());
+        assertEquals(plan.getType(), responseDTO.getType());
+        assertEquals(plan.getMonthlyPrice(), responseDTO.getMonthlyPrice());
+        assertEquals(plan.getCurrency().getCurrencyCode(), responseDTO.getCurrency());
+        assertEquals(plan.getDurationMonths(), responseDTO.getDurationMonths());
+        assertEquals(plan.getMaxMembers(), responseDTO.getMaxMembers());
+        assertNotNull(responseDTO.getGymDTO());
+
+        verify(gymRepository, times(1)).existsById(gymId);
+        verify(membershipPlanRepository, times(1)).findByGymId(gymId);
+        verify(gymService, times(1)).convertEntityToDTO(gym);
+    }
+
+    // test for exception EntityNotFoundException in method 'getAllPlansForGym' when gym is not found
+    @Test
+    void getAllPlansForGym_ShouldThrowEntityNotFoundException() {
+        // GIVEN
+        Long gymId = 1L;
+
+        when(gymRepository.existsById(gymId)).thenReturn(false);
+
+        // WHEN
+        EntityNotFoundException exception = assertThrows(EntityNotFoundException.class,
+                () -> membershipPlanService.getAllPlansForGym(gymId));
+        // THEN
+        assertEquals("Gym with ID: " + gymId + " is not found", exception.getMessage());
+        verify(gymRepository, times(1)).existsById(gymId);
+        verify(membershipPlanRepository, never()).findByGymId(anyLong());
+        verify(gymService, never()).convertEntityToDTO(any(Gym.class));
+    }
+
+    // test for success method 'convertEntityToDTO'
+    @Test
+    void convertEntityToDTO_ShouldReturnMembershipPlanDTO() {
+        // GIVEN
+        Gym gym = createGym();
+        GymDTO gymDTO = createGymDTO();
+        MembershipPlan plan = createMembershipPlan(gym);
+        when(gymService.convertEntityToDTO(any(Gym.class))).thenReturn(gymDTO);
+
+        // WHEN
+        MembershipPlanDTO response = membershipPlanService.convertEntityToDTO(plan);
+
+        // THEN
+        assertNotNull(response);
+        assertEquals(plan.getId(), response.getId());
+        assertEquals(plan.getName(), response.getName());
+        assertEquals(plan.getType(), response.getType());
+        assertEquals(plan.getMonthlyPrice(), response.getMonthlyPrice());
+        assertEquals(plan.getCurrency().getCurrencyCode(), response.getCurrency());
+        assertEquals(plan.getDurationMonths(), response.getDurationMonths());
+        assertEquals(plan.getMaxMembers(), response.getMaxMembers());
+
+        assertNotNull(response.getGymDTO());
+        assertEquals(gymDTO.getName(), response.getGymDTO().getName());
+
+        verify(gymService, times(1)).convertEntityToDTO(any(Gym.class));
+    }
+
+    // test for success method 'convertDTOtoEntity'
+    @Test
+    void convertDTOtoEntity_ShouldReturnMembershipPlan() {
+        // GIVEN
+        MembershipPlanDTO planDTO = createMembershipPlanDTO();
+        // WHEN
+        MembershipPlan response = membershipPlanService.convertDTOtoEntity(planDTO);
+        // THEN
+        assertNotNull(response);
+        assertEquals(planDTO.getName(), response.getName());
+        assertEquals(planDTO.getType(), response.getType());
+        assertEquals(planDTO.getMonthlyPrice(), response.getMonthlyPrice());
+        assertEquals(planDTO.getCurrency(), response.getCurrency().getCurrencyCode());
+        assertEquals(planDTO.getDurationMonths(), response.getDurationMonths());
+        assertEquals(planDTO.getMaxMembers(), response.getMaxMembers());
     }
 }
